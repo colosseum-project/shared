a5a0f3c96f61b51a512af6976a4049e4319832a5
Maxence <maxgrymonprez@live.fr>
2024-02-09 16:34:37 +0100

    Add namespace definition as YAML

--- kubernetes/app.ps1
+++ kubernetes/app.ps1
@@ -5,8 +5,6 @@ param (
     [string] $Action = "apply"
 )
 
-& kubectl.exe create namespace sandbox
-
 Get-ChildItem -Path $PSScriptRoot\resources -File -Filter "*.yaml" | ForEach-Object -Process {
     & kubectl.exe $Action -f $_.FullName
 }

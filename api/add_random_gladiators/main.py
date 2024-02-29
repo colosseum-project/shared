f2e27ba9a1193737bd30e2f639b2261fe3ab44df
Maxence <maxgrymonprez@live.fr>
2024-02-29 10:44:30 +0100

    Enhance add_random_gladiators script Add argparser to Python script Add instructions to use this script for the sandbox k8s

--- api/add_random_gladiators/main.py
+++ api/add_random_gladiators/main.py
@@ -1,11 +1,29 @@
 import os
 import sys
-import requests
+import argparse
 from random import randint
 
-HOST = "localhost"
-PORT = "8081"
-GLADIATORS_COUNT = 20
+try:
+    import requests
+except ImportError:
+    print(
+        "Missing 'requests' module, "
+        "please run `python -m pip install requests` to install it."
+    )
+    raise
+
+parser = argparse.ArgumentParser(description="Add randomly-named gladiators to Ludus.")
+parser.add_argument(
+    "host", default="localhost", type=str, help="Hostname of targeted Ludus service"
+)
+parser.add_argument(
+    "-p", "--port", default="8081", type=int, help="Port of targeted Ludus service"
+)
+parser.add_argument(
+    "-n", "--number", default="20", type=int, help="Number of gladiator to add"
+)
+
+args = parser.parse_args()
 
 script_root = os.path.dirname(os.path.abspath(sys.argv[0]))
 latin_names_file = os.path.join(script_root, "latin_names.txt")
@@ -13,16 +31,21 @@ latin_names = open(latin_names_file, "r").read().splitlines()
 latin_names_count = len(latin_names)
 gladiator_types = ("MURMILLO", "THRAEX", "DIMACHAERUS")
 gladiator_types_count = len(gladiator_types)
-for i in range(GLADIATORS_COUNT):
+for i in range(args.number):
     gladiator = {
         "name": latin_names[randint(0, latin_names_count - 1)],
         "type": gladiator_types[randint(0, gladiator_types_count - 1)],
     }
-    requests.post(
-        url=f"http://{HOST}:{PORT}/gladiators",
+    r = requests.post(
+        url=f"http://{args.host}:{args.port}/gladiators",
         json=gladiator,
     )
-    print(
-        f"{gladiator['type'].capitalize()} {gladiator['name']}",
-        "has been added to Ludus",
-    )
+    if r.status_code == 200:
+        print(
+            f"{gladiator['type'].capitalize()} {gladiator['name']}",
+            "has been added to Ludus",
+        )
+    else:
+        raise requests.RequestException(
+            f"Could not add gladiator(s), returns error code {r.status_code}."
+        )
--- kubernetes/README.md
+++ kubernetes/README.md
@@ -1,12 +1,21 @@
 # Deploy on Kubernetes
 
+- [Deploy on Kubernetes](#deploy-on-kubernetes)
+  - [Create the K8s cluster](#create-the-k8s-cluster)
+  - [Deploy the applicaton](#deploy-the-applicaton)
+  - [Clean everything up](#clean-everything-up)
+
+## Create the K8s cluster
+
 Here, we use [`kind`](https://kind.sigs.k8s.io/) to deploy our K8s sandbox environment on Docker.
 
-Use this command to create the `kind` cluster:
+Use this command to create the cluster:
 
 ```sh
 kind create cluster --config ./kind_config.yaml
 ```
+
+## Deploy the applicaton
 
 To deploy the application inside the cluster, run:
 
@@ -16,12 +25,28 @@ kubectl apply -f ./resources
 
 After a short moment, the Bisselium WebUI interface can be accessed via <http://127.0.0.1:30000/>.
 
-The Ludus endpoint is also accessible via <http://127.0.0.1:30001/>. This allows you to manage gladiators and engage combats.
+The Ludus endpoint is also accessible via <http://127.0.0.1:30001/>. _This allows you to manage gladiators and engage them in combats._
+
+Add gladiators by running this Python script:
+
+```sh
+python ./api/add_random_gladiators/main.py 127.0.0.1 -p 30001
+```
+
+Engage random duels with this cURL command:
+
+```sh
+curl -X POST http://127.0.0.1:30001/duels/resolve/random
+```
+
+## Clean everything up
 
 Once you are done, you can remove the cluster with this single command:
 
 ```sh
 kind delete cluster colosseum-project
 ```
+
+---
 
 Have fun! :)
